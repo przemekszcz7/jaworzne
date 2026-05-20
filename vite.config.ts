e2ba0f420aa850e4328be9f6bc0da93bb4d881a1
przemekszcz7 <przemekszcz7@gmail.com>
2026-05-20 09:01:02 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(({ mode }) => {
   return {
-    base: mode === 'production' ? '/smazalniajaworze/' : '/',
+    base: mode === 'production' ? '/jaworzne/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
